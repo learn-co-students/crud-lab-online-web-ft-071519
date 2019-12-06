f319828bc562dc440a63c4c233a2661729a37b2a
github username <github email address>
2019-12-06 20:36:00 +0000

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+state = {
+  text: ""
+}
+
+handleOnChange = (event) => {
+  console.log(this.state)
+  this.setState({
+    text: event.target.value
+  })
+}
+
+handleOnSubmit = (event) => {
+  event.preventDefault()
+  this.props.addRestaurant(this.state.text)
+  this.setState({
+    text: ""
+  })
+}
+
   render() {
+    console.log(this.props)
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleOnSubmit}>
+          <input onChange={this.handleOnChange} type="text" value={this.state.text}/>
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
+  renderReviews = () => {
+    const restaurantRiviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId);
+       return restaurantRiviews.map(review => <Review deleteReview={this.props.deleteReview} key={review.id} review={review}/>)
+}
+
+
   render() {
+    console.log(this.props)
     return (
       <ul>
         Reviews
+        {this.renderReviews()}
       </ul>
     );
   }
